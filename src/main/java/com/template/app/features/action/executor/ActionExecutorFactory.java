5eda0177cca37752d762a354049d7a40ff569c6d
Pc_Huy <pchuy4003@gmai.com>
2026-01-26 22:09:23 +0700

    feat: fix warning

--- src/main/java/com/template/app/features/action/executor/ActionExecutorFactory.java
+++ src/main/java/com/template/app/features/action/executor/ActionExecutorFactory.java
@@ -15,12 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class ActionExecutorFactory {
-    
-    private final Map<Class<? extends BaseActionConfig>, ActionExecutor> executorMap;
+
+    private final Map<Class<? extends BaseActionConfig>, ActionExecutor<?>> executorMap;
 
     public ActionExecutorFactory(List<ActionExecutor<?>> executors) {
         this.executorMap = executors.stream()
-            .collect(Collectors.toMap(ActionExecutor::getSupportedConfigClass, Function.identity()));
+                .collect(Collectors.toMap(ActionExecutor::getSupportedConfigClass, Function.identity()));
     }
 
     public void executeAction(BaseActionConfig config, BaseActionContext context) {
@@ -29,14 +29,30 @@ public class ActionExecutorFactory {
             return;
         }
 
-        ActionExecutor executor = executorMap.get(config.getClass());
+        ActionExecutor<?> executor = executorMap.get(config.getClass());
 
         if (executor == null) {
             log.error("No executor found for config type: {}", config.getClass().getName());
             return;
         }
 
-        log.info(">>> Executing Action [{}] for Context [{}]", config.getClass().getSimpleName(), context.getTriggerSource());
-        executor.execute(config, context);
+        log.info(">>> Executing Action [{}] for Context [{}]", config.getClass().getSimpleName(),
+                context.getTriggerSource());
+
+        executeHelper(executor, config, context);
+    }
+
+    private <T extends BaseActionConfig> void executeHelper(ActionExecutor<T> executor, BaseActionConfig config,
+            BaseActionContext context) {
+        Class<T> configClass = executor.getSupportedConfigClass();
+
+        if (configClass.isInstance(config)) {
+            T castedConfig = configClass.cast(config);
+
+            executor.execute(castedConfig, context);
+        } else {
+            log.error("Type Mismatch! Executor expects {} but got {}", configClass.getName(),
+                    config.getClass().getName());
+        }
     }
 }
